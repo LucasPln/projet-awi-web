0f571a7b342994b40b6a9ae3845a9a7ff9a0cfaf
LucasPln <lucas.paulin@hotmail.fr>
2020-03-03 16:00:05 +0100

    Composant Post fini (élement sans CSS)

--- src/components/Post.js
+++ src/components/Post.js
@@ -9,13 +9,17 @@ class Post extends Component{
             <div id="post">
                 <h2 id="pseudo-post">{this.props.post.createur}</h2>
                 <p id="text-post">{this.props.post.texte}</p>
+                <input id="button_like" type="button" value="Like" />
+                <a id="comment" href="" >Commentaire</a>
+                <input id="button_signaler" type="button" value="Signaler" />
+                
             </div>
         )
     }
 }
+
 const mapStateToProps = state => ({
     
-
 })
 
 
